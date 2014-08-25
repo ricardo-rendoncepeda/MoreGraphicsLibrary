9d11b08b29dfae05ca5be2c49327886fad32c178
Ricardo Rendon Cepeda <ricardo.rendon.cepeda@idean.com>
2014-08-24 18:16:32 -0700

    Added MGLPolygon

--- Xcode/MoreGraphicsLibrary/MoreGraphicsLibrary/MGLTestView.m
+++ Xcode/MoreGraphicsLibrary/MoreGraphicsLibrary/MGLTestView.m
@@ -13,8 +13,9 @@
 
 - (void)shadeRect:(CGRect)rect
 {
-    [self testRectWithRect:rect];
+//    [self testRectWithRect:rect];
 //    [self testCurveWithRect:rect];
+    [self testPolygonWithRect:rect];
 }
 
 - (void)testRectWithRect:(CGRect)rect
@@ -41,7 +42,7 @@
     MGLPoint* c = [MGLPoint pointWithX:600.0 y:400.0];
     MGLPoint* end = [MGLPoint pointWithX:400.0 y:200.0];
     
-    MGLCurve* mglCurve = [[MGLCurve alloc] initWithLine:[MGLLine lineWithStart:start end:end] inFrame:rect];
+    MGLCurve* mglCurve = [[MGLCurve alloc] initWithLine:[MGLLine lineWithStart:start end:end] inFrame:self.frame];
     
     mglCurve.segments = 50;
     mglCurve.strokeWidth = 8.0;
@@ -50,6 +51,28 @@
     [mglCurve bezierQuarticWithPointA:a b:b c:c];
     [mglCurve strokeBezier];
     [mglCurve showPoints];
+}
+
+- (void)testPolygonWithRect:(CGRect)rect
+{
+    MGLPolygon* mglPolygon = [[MGLPolygon alloc] initWithPoint:[MGLPoint pointWithX:0.0 y:100.0] inFrame:self.frame];
+    
+    [mglPolygon addPoint:[MGLPoint pointWithX:60.0 y:0.0]];
+    [mglPolygon addPoint:[MGLPoint pointWithX:100.0 y:80.0]];
+    [mglPolygon addPoint:[MGLPoint pointWithX:140.0 y:40.0]];
+    [mglPolygon addPoint:[MGLPoint pointWithX:200.0 y:140.0]];
+    [mglPolygon addPoint:[MGLPoint pointWithX:140.0 y:120.0]];
+    [mglPolygon addPoint:[MGLPoint pointWithX:120.0 y:200.0]];
+    [mglPolygon addPoint:[MGLPoint pointWithX:80.0 y:60.0]];
+    [mglPolygon addPoint:[MGLPoint pointWithX:20.0 y:120.0]];
+    [mglPolygon addPoint:[MGLPoint pointWithX:40.0 y:180.0]];
+    
+    mglPolygon.fillColor = [MGLColor colorWithRed:0.0 green:1.0 blue:1.0 alpha:1.0];
+    [mglPolygon fillPolygon];
+    
+    mglPolygon.strokeColor = [MGLColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:1.0];
+    mglPolygon.strokeWidth = 4.0;
+    [mglPolygon strokePolygon];
 }
 
 @end
